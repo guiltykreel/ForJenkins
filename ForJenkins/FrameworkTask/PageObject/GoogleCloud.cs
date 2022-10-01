dc73c2188c52baf14a784732fdd873ef6499d77d
Aleh Rusevich <prokazzaangel@gmail.com>
2022-10-01 17:37:21 +0300

    FrameworkTask for jenkins

---
+++ ForJenkins/FrameworkTask/PageObject/GoogleCloud.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace FrameworkTask
+{
+    public class GoogleCloud
+    {
+        private string _url = "https://cloud.google.com/";
+        private WebDriver driver;
+        private WebDriverWait wait;
+        private By searchButtonLocator = By.ClassName("devsite-searchbox");
+        //private By searchLocator= By.CssSelector("div.devsite-search-container.devsite-search-query");
+        private By searchLocator = By.ClassName("devsite-search-field");
+        private By calculatorRefLocator = By.LinkText("Google Cloud Pricing Calculator");//"https://cloud.google.com/products/calculator
+        
+        public IWebElement SearchButton => driver.FindElement(searchButtonLocator);
+        public IWebElement CookieNotification => driver.FindElement(By.XPath("//devsite-snackbar[@type='cookie-notification']/div"));
+        public IWebElement AcceptCookies => driver.FindElement(By.XPath("//button[@class='devsite-snackbar-action']"));
+       
+        public GoogleCloud(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public GoogleCloud openPage()
+        {
+            driver.Navigate().GoToUrl(_url);
+            driver.Manage().Window.Maximize();
+            return this;
+        }
+
+        public GoogleCloud Search(string searchRequest)
+        {
+            SearchButton.Click();
+            driver.FindElement(searchLocator).SendKeys(searchRequest + Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 3);
+            return this;
+        }
+
+        public CloudCalculator OpenCalculator()
+        {
+            driver.FindElement(calculatorRefLocator).Click();
+            return new CloudCalculator(driver);
+        }
+
+        public GoogleCloud AllowCookies()
+        {
+            AcceptCookies.Click();
+            return this;
+        }
+    }
+}
---
+++ ForJenkins/FrameworkTask/PageObject/TempMailPage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace FrameworkTask
+{
+    public class TempMailPage
+    {
+        private string _url = "https://tempmail.plus";
+        private WebDriver driver;
+        private By costLocator = By.XPath("//*[contains(text(), 'Estimated Monthly Cost:')]");
+
+        private IWebElement RecievedEmail => driver.FindElement(By.ClassName("mail"));
+        private IWebElement CopyEmailButton => driver.FindElement(By.Id("pre_copy"));
+
+        public TempMailPage(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public TempMailPage OpenPage()
+        {
+            driver.ExecuteScript("window.open()");
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            driver.Navigate().GoToUrl(_url);
+            return this;
+        }
+
+        public TempMailPage GetTempEmail()
+        {
+            driver.Manage().Timeouts().ImplicitWait = new TimeSpan(0, 0, 3);
+            CopyEmailButton.Click();
+            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            return this;
+        }
+
+        public string GetMailedCost()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 60));
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            wait.Until(ExpectedConditions.ElementExists(By.ClassName("mail"))).Click();
+            RecievedEmail.Click();
+            driver.ExecuteScript("arguments[0].scrollIntoView(true);", RecievedEmail);
+            return driver.FindElement(costLocator).Text;
+        }
+    }
+}
